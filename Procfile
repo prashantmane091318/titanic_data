754c03c496d95d60a9950b6c845f64fcd6d876f7
prashantmane091318 <61352956+prashantmane091318@users.noreply.github.com>
2020-06-14 05:51:29 -0700

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 17:49:22 2020
+
+@author: Pradipkumar
+"""
+
+web:gunicorn flask_app:app
---
+++ flask_app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 14:03:40 2020
+
+@author: Pradipkumar
+"""
+
+from flask import Flask,render_template,request
+import pickle
+app=Flask(__name__)
+
+@app.route("/")
+def fun1():
+    return render_template("titanic.html") # it provides html page as response
+
+@app.route("/predict",methods=["POST"])
+def fun2():
+    Pclass=float(request.form["PClass"])
+    Age=float(request.form["Age"])
+    SibSp=float(request.form["SibSp"])
+    Parch=float(request.form["Parch"])
+    Fare=float(request.form["Fare"])
+    Sex_male=float(request.form["Sex_male"])
+    Embarked_Q=float(request.form["Embarked_Q"])
+    Embarked_S=float(request.form["Embarked_S"])
+    mymodel=pickle.load(open("mymodel1.pkl","rb"))
+    survival=mymodel.predict([[Pclass, Age, SibSp, Parch, Fare, Sex_male, Embarked_Q,Embarked_S]])[0]
+    return "Survival of Person is {}".format(survival)
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
+    
+    
